f8a80c676dafb36b76d6a8a94b476dba41958092
fizabells22 <fizabella.ru@gmail.com>
2022-03-13 21:00:26 +0700

    Add Google Maps Flutter

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ParcelAppTheme.lightTheme,
       home: const Scaffold(
-        body: SendParcelCheckoutScreen(),
+        body: ParcelCenterScreen(),
         bottomNavigationBar: MyBottomNavigationBar(),
       ),
     );
